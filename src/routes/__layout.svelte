f4cd7bf11039441610e0f578b5cc9aa47094cfa6
W Kirk Beaty <w.kirk.beaty@gmail.com>
2021-07-13 14:12:36 -0400

    another commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,20 @@
+<script>
+import Nav from "../components/nav.svelte";
+
+</script>
+
+<style global lang="postcss">
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
+
+<div class="p-8 max-w-6xl mx-auto">
+    <Nav/>
+    <slot>
+    </slot>
+</div>
+
+
+
+
---
+++ src/routes/about.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p class="text-center">This was a fun app to develop that uses Svelte Kit to build a Pokedex web application.</p>
